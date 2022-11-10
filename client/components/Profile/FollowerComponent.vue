c65c5c5f806c21040f60aff0a33865a96165a2aa
Benjamin Gao <bgao01@mit.edu>
2022-11-09 19:55:49 -0500

    list out followers

---
+++ client/components/Profile/FollowerComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>
+    @{{ this.follower ? this.follow.follower : this.follow.followee }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'FollowerComponent',
+  props: {
+    // Data from the stored follow
+    follow: {
+      type: Object,
+      required: true
+    },
+    // boolean for if we are displaying the user's follower or who they are following
+    follower: {
+      type: Boolean,
+      required: true
+    }
+  },
+  mounted() {
+    console.log("following username is:", this.follow.follower);
+  },
+  methods: {
+    
+  }
+};
+</script>
+  
+<style scoped>
+p {
+  margin: 0px;
+}
+</style>
+  
--- client/components/Profile/ProfilePage.vue
+++ client/components/Profile/ProfilePage.vue
@@ -1,135 +1,170 @@
 <template>
-    <main>
-      <section v-if="$route.params.username">
-        <header>
-          <div class="left">
-            <h1>
-              Viewing {{ $route.params.username }}'s feed
-              <span v-if="$store.state.filter">
-                by @{{ $store.state.filter }}
-              </span>
-            </h1>
-          </div>
-          <div class="right">
-            <GetFreetsForm
-              ref="getFreetsForm"
-              value="author"
-              placeholder="🔍 Filter by author (optional)"
-              button="🔄 Get freets"
-            />
-          </div>
-        </header>
-
-        <div id="depolarize" v-if="$store.state.username === $route.params.username">
-          <div>
-            Depolarize Value: {{this.$store.state.depolarize}}
-          </div>
-          <button 
-            @click="toggleDepolarizeValue"
-          >
-            Toggle Depolarize Value
-          </button>
+  <main>
+    <section v-if="$route.params.username">
+      <header>
+        <div>
+          <h1>
+            Viewing {{ $route.params.username }}'s feed
+            <span v-if="$store.state.filter">
+              by @{{ $store.state.filter }}
+            </span>
+          </h1>
         </div>
-
-        <div class="follow">
-          <div class="left">
-            {{ this.$store.state.followers.length }} {{ this.$store.state.followers.length == 1 ? "Follower" : "Followers"}}
-          </div>
-          <div class="right">
-            {{ this.$store.state.following.length }} {{ "Following"}}
-
-          </div>
-        </div>
-
-        <section
-          v-if="$store.state.feed.length"
-        >
-          <FreetComponent
-            v-for="freet in $store.state.feed"
-            :key="freet.id"
-            :freet="freet"
+        <div>
+          <GetFreetsForm
+            ref="getFreetsForm"
+            value="author"
+            placeholder="🔍 Filter by author (optional)"
+            button="🔄 Get freets"
           />
-        </section>
-        <article
-          v-else
+        </div>
+      </header>
+
+      <div id="depolarize" v-if="$store.state.username === $route.params.username">
+        <div>
+          Depolarize Value: {{this.$store.state.depolarize}}
+        </div>
+        <button 
+          @click="toggleDepolarizeValue"
         >
-          <h3>{{ $route.params.username }}'s feed is empty.</h3>
-        </article>
+          Toggle Depolarize Value
+        </button>
+      </div>
+
+      <div class="follow">
+        <div class="left">
+          <p id="followTitle">
+            {{ this.$store.state.followers.length }} {{ this.$store.state.followers.length == 1 ? "Follower" : "Followers"}}
+          </p>
+          <!-- {{ this.$store.state.followers.length }} {{ this.$store.state.followers.length == 1 ? "Follower" : "Followers"}} -->
+          <section id="followers"
+            v-if="$store.state.followers.length"
+          >
+            <FollowerComponent
+              v-for="follow in $store.state.followers"
+              :key="follow.id"
+              :follow="follow"
+              :follower="true"
+            />
+          </section>
+          <!-- <article
+            v-else
+          >
+            <h3>No followers found.</h3>
+          </article> -->
+        </div>
+        <div class="right">
+          <p id="followTitle">
+            {{ this.$store.state.following.length }} {{ "Following"}}
+          </p>
+          <section id="following"
+            v-if="$store.state.following.length"
+          >
+            <FollowerComponent
+              v-for="follow in $store.state.following"
+              :key="follow.id"
+              :follow="follow"
+              :follower="false"
+            />
+          </section>
+          <!-- <article
+            v-else
+          >
+            <h3>No followers found.</h3>
+          </article> -->
+        </div>
+      </div>
+
+      <section
+        v-if="$store.state.feed.length"
+      >
+        <FreetComponent
+          v-for="freet in $store.state.feed"
+          :key="freet.id"
+          :freet="freet"
+        />
       </section>
-    </main>
-  </template>
+      <article
+        v-else
+      >
+        <h3>{{ $route.params.username }}'s feed is empty.</h3>
+      </article>
+    </section>
+  </main>
+</template>
   
-  <script>
-  import FreetComponent from '@/components/Freet/FreetComponent.vue';
-  import GetFreetsForm from '@/components/Freet/GetFreetsForm.vue';
-  
-  export default {
-    name: 'FreetPage',
-    components: {FreetComponent, GetFreetsForm},
-    mounted() {
-      this.$store.commit("refreshFeed", this.$route.params.username);
-      this.$store.commit("refreshFollowers", this.$route.params.username);
-      this.$store.commit("refreshFollowing", this.$route.params.username);
-      console.log("feed:", this.$store.state.feed);
-      console.log("freets:", this.$store.state.freets);
-      console.log("profile followers:", this.$store.state.followers);
-      console.log("profile following:", this.$store.state.following);
+<script>
+import FreetComponent from '@/components/Freet/FreetComponent.vue';
+import GetFreetsForm from '@/components/Freet/GetFreetsForm.vue';
+import FollowerComponent from '@/components/Profile/FollowerComponent.vue';
+
+export default {
+  name: 'FreetPage',
+  components: {FreetComponent, GetFreetsForm, FollowerComponent},
+  mounted() {
+    this.$store.commit("refreshFeed", this.$route.params.username);
+    this.$store.commit("refreshFollowers", this.$route.params.username);
+    this.$store.commit("refreshFollowing", this.$route.params.username);
+    console.log("feed:", this.$store.state.feed);
+    console.log("freets:", this.$store.state.freets);
+    console.log("profile followers:", this.$store.state.followers);
+    console.log("profile following:", this.$store.state.following);
+  },
+  methods: {
+    toggleDepolarizeValue() {
+      const params = {
+        method: 'PATCH',
+        body: JSON.stringify({depolarize: !this.$store.state.depolarize}),
+        callback: () => {
+          this.$store.commit('alert', {
+            message: 'Successfully changed Depolarize value!', status: 'success'
+          });
+          // Refresh the feed after successfully changing the depolarize value
+          this.$store.commit("refreshFeed", this.$route.params.username);
+        }
+      };
+      this.requestDepolarize(params);
     },
-    methods: {
-      toggleDepolarizeValue() {
-        const params = {
-          method: 'PATCH',
-          body: JSON.stringify({depolarize: !this.$store.state.depolarize}),
-          callback: () => {
-            this.$store.commit('alert', {
-              message: 'Successfully changed Depolarize value!', status: 'success'
-            });
-            // Refresh the feed after successfully changing the depolarize value
-            this.$store.commit("refreshFeed", this.$route.params.username);
-          }
-        };
-        this.requestDepolarize(params);
-      },
-      async requestDepolarize(params) {
-        /**
-         * Submits a request to the depolarize endpoint
-         * @param params - Options for the request
-         * @param params.body - Body for the request, if it exists
-         * @param params.callback - Function to run if the the request succeeds
-         */
-        const options = {
-          method: params.method, headers: {'Content-Type': 'application/json'}
-        };
-        if (params.body) {
-          options.body = params.body;
+    async requestDepolarize(params) {
+      /**
+       * Submits a request to the depolarize endpoint
+       * @param params - Options for the request
+       * @param params.body - Body for the request, if it exists
+       * @param params.callback - Function to run if the the request succeeds
+       */
+      const options = {
+        method: params.method, headers: {'Content-Type': 'application/json'}
+      };
+      if (params.body) {
+        options.body = params.body;
+      }
+
+      try {
+        let r;
+        console.log(options);
+        r = await fetch(`/api/users`, options);
+        
+        if (!r.ok) {
+          console.log("Failed to change depolarize value");
+          const res = await r.json();
+          console.log("res is:", res);
+        } else {
+          const res = await r.json();
+          console.log("res from depolarize:", res);
+          this.$store.commit("setDepolarize", res.user.depolarize)
+          console.log("new depolarize Value is:", this.$store.state.depolarize)
         }
 
-        try {
-          let r;
-          console.log(options);
-          r = await fetch(`/api/users`, options);
-          
-          if (!r.ok) {
-            console.log("Failed to change depolarize value");
-            const res = await r.json();
-            console.log("res is:", res);
-          } else {
-            const res = await r.json();
-            console.log("res from depolarize:", res);
-            this.$store.commit("setDepolarize", res.user.depolarize)
-            console.log("new depolarize Value is:", this.$store.state.depolarize)
-          }
-
-          params.callback();
-        } catch (e) {
-          console.log("error in depolarize request:", e)
-        }
-      },
-    }
-  };
-  </script>
+        params.callback();
+      } catch (e) {
+        console.log("error in depolarize request:", e)
+      }
+    },
+  }
+};
+</script>
   
-  <style scoped>
+<style scoped>
   section {
     display: flex;
     flex-direction: column;
@@ -157,11 +192,30 @@
 
   .follow {
     display: flex;
+    flex-direction: row;
     justify-content: space-between;
     align-items: center;
     margin-top: 10px;
     margin-bottom: 10px;
+    
 
+  }
+
+  .followTitle {
+    text-decoration: underline;
+  }
+
+  .left {
+    margin-top: 0;
+    vertical-align: text-top;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    vertical-align: text-top;
+    /* align-items:; */
+    /* border: 1px solid #111;
+    padding: 0.5rem;
+    border-radius: 20px; */
   }
 
   #depolarize {
@@ -169,5 +223,14 @@
     margin-bottom: 10px;
   }
 
-  </style>
+  #following {
+    margin-top: 5px;
+  }
+
+  #followers {
+    margin-top: 5px;
+
+  }
+
+</style>
   
